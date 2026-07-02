7632245ec24b504b01440205ad83cacd06478aee
bch <brandon.howe@mechsol.com>
2026-07-02 17:05:02 -0400

    Add new recording testproject

---
+++ Solution/RecordingTest/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace RecordingTest
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
---
+++ Solution/RecordingTest/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace RecordingTest
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
