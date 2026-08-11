247aa44eedb1ca17a9166f7c31491e06c500aa83
SVF <svf@mechsol.com>
2026-08-11 11:38:37 -0400

    Add foi video controls

--- Solution/PhoneApp/Views/AnalysisPage/VideoView.xaml.cs
+++ Solution/PhoneApp/Views/AnalysisPage/VideoView.xaml.cs
@@ -1,8 +1,14 @@
+using CommunityToolkit.Maui.Core;
+using MicroVue.ViewModels;
+using System.Diagnostics;
+
 namespace MicroVue.Views;
 
 public partial class VideoView : ContentView
 {
-	public VideoView()
+    bool isDragging = false;
+
+    public VideoView()
 	{
 		InitializeComponent();
 	}
@@ -13,4 +19,55 @@ public partial class VideoView : ContentView
         if (bottomPanel != null) bottomPanel.IsVisible = width <= height;
         if (sidePanel != null) sidePanel.IsVisible = width > height;
     }
+
+    void Player_PropertyChanged(object? sender, System.ComponentModel.PropertyChangedEventArgs args)
+    {
+        if (player != null)
+        {
+            if (args.PropertyName == nameof(player.Duration))
+            {
+                slider.Maximum = player.Duration.TotalSeconds;
+                Debug.WriteLine($"[Debug]: Player duration {player.Duration.TotalSeconds}");
+            }
+        }
+    }
+
+    void Player_PositionChanged(object sender, MediaPositionChangedEventArgs args)
+    {
+        Debug.WriteLine($"[Debug]: Player position changed: {args.Position.TotalSeconds}");
+        if (!isDragging)
+        {
+            slider.Value = args.Position.TotalSeconds;
+        }
+    }
+
+    void OnPlayPauseButtonClicked(object sender, EventArgs args)
+    {
+        if (player.CurrentState == MediaElementState.Stopped ||
+            player.CurrentState == MediaElementState.Paused)
+        {
+            player.Play();
+        }
+        else if (player.CurrentState == MediaElementState.Playing)
+        {
+            player.Pause();
+        }
+    }
+
+    void OnSliderDragStarted(object sender, EventArgs e)
+    {
+        isDragging = true;
+    }
+
+    async void OnSliderDragCompleted(object sender, EventArgs e)
+    {
+        if (sender is Slider slider)
+        {
+            var targetPosition = TimeSpan.FromSeconds(slider.Value);
+
+            await player.SeekTo(targetPosition, CancellationToken.None);
+            isDragging = false;
+        }
+    }
+
 }
