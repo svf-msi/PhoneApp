016e0280a9c8f5d9409d5dc967b48136d1822f42
SVF <svf@mechsol.com>
2026-08-27 10:55:24 -0400

    Update version number

--- Solution/PhoneApp/Views/AboutPage.xaml.cs
+++ Solution/PhoneApp/Views/AboutPage.xaml.cs
@@ -5,5 +5,6 @@ public partial class AboutPage : ContentPage
 	public AboutPage()
 	{
 		InitializeComponent();
-	}
+        version_label.Text = $"MicroVue™ version {AppInfo.Current.VersionString}";
+    }
 }
