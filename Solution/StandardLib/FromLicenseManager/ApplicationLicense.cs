90e1303b5d4cb5f5de34cdb274be572e1a0dc0c0
svf <svf@mechsol.com>
2026-08-27 17:34:41 -0400

    Add license manager lib

---
+++ Solution/StandardLib/FromLicenseManager/ApplicationLicense.cs
@@ -0,0 +1,262 @@
+﻿using Standard.Licensing;
+using Standard.Licensing.Validation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LicenseManager.Library
+{
+    public class ApplicationLicense
+    {
+        public static void CreateKeys(string password, out string privateKey, out string publicKey)
+        {
+            var keyGenerator = Standard.Licensing.Security.Cryptography.KeyGenerator.Create();
+            var keyPair = keyGenerator.GenerateKeyPair();
+            privateKey = keyPair.ToEncryptedPrivateKeyString(password);
+            publicKey = keyPair.ToPublicKeyString();
+            Console.WriteLine($"Pass phrase: {password} \nPrivate key: {privateKey} \nPublic key: {publicKey}");
+        }
+
+        public static void SaveKeys(string privateKey, string publicKey, string file)
+        {
+            File.WriteAllLines(file, new string[] { privateKey, publicKey });
+        }
+
+        public static bool ReadKeys(string file, out string privateKey, out string publicKey)
+        {
+            privateKey = "";
+            publicKey = "";
+
+            if (File.Exists(file))
+            {
+                var keys = File.ReadAllLines(file);
+                if (keys.Length > 1)
+                {
+                    privateKey = keys[0];
+                    publicKey = keys[1];
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public static string GetHardwareId()
+        {
+            try
+            {
+                var deviceId = GetDeviceId();
+                if (string.IsNullOrWhiteSpace(deviceId)) return "Failed to identify device ID";
+                else return Hash(deviceId);
+            }
+            catch (Exception e)
+            {
+                return "Failed to identify hardware ID";
+            }
+        }
+
+        public static string GetDeviceId()
+        {
+#if ANDROID
+    // Returns the 64-bit Android ID (Settings.Secure.ANDROID_ID)
+    var context = Android.App.Application.Context;
+    return Android.Provider.Settings.Secure.GetString(context.ContentResolver, Android.Provider.Settings.Secure.AndroidId);
+    
+#elif IOS
+    // Returns the alphanumeric string unique to the device and vendor
+    return UIKit.UIDevice.CurrentDevice.IdentifierForVendor?.ToString() ?? string.Empty;
+    
+#elif WINDOWS
+    // Returns a unique hardware-based system ID for the publisher
+    var systemId = Microsoft.System.GetSystemIdForPublisher();
+    return Windows.Security.Cryptography.CryptographicBuffer.EncodeToHexString(systemId.Id);
+    
+#else
+            return string.Empty;
+#endif
+        }
+
+        static string Hash(string input)
+        {
+            using (var sha1 = new SHA1Managed())
+            {
+                var hash = sha1.ComputeHash(Encoding.UTF8.GetBytes(input));
+                var sb = new StringBuilder(hash.Length * 2);
+
+                foreach (var b in hash) // can be "x2" if you want lowercase
+                    sb.Append(b.ToString("X2"));
+
+                return sb.ToString();
+            }
+        }
+
+        public static License CreateLicense(string privateKey, string password, LicenseParameters attributes)
+        {
+            var license = License.New().WithUniqueIdentifier(Guid.NewGuid());
+
+            if (attributes != null)
+            {
+                if (attributes.Type == LicenseType.Trial)
+                {
+                    license = license.As(LicenseType.Trial).ExpiresAt(DateTime.Now.AddDays(attributes.Expiration));
+                }
+                else
+                {
+                    license = license.As(LicenseType.Standard);
+                }
+
+                PopulateFeatures(license, attributes);
+                PopulateAddons(license, attributes);
+
+                if (!string.IsNullOrEmpty(attributes.Customer))
+                {
+                    license = license.LicensedTo(attributes.Customer, attributes.Email ?? "");
+                }
+            }
+
+            return license.CreateAndSignWithPrivateKey(privateKey, password);
+        }
+
+        static void PopulateFeatures(ILicenseBuilder license, LicenseParameters attributes)
+        {
+            if (attributes != null)
+            {
+                var dict = new Dictionary<string, string>();
+                Populate(dict, nameof(attributes.Module), attributes.Module);
+                Populate(dict, nameof(attributes.Package), attributes.Package);
+                Populate(dict, nameof(attributes.Version), attributes.Version);
+
+                if (dict.Count > 0)
+                {
+                    license = license.WithProductFeatures(dict);
+                }
+            }
+        }
+
+        static void PopulateAddons(ILicenseBuilder license, LicenseParameters attributes)
+        {
+            if (attributes != null)
+            {
+                var dict = new Dictionary<string, string>();
+                Populate(dict, nameof(attributes.Host), attributes.Host);
+                Populate(dict, nameof(attributes.StartDate), attributes.StartDate);
+                Populate(dict, nameof(attributes.EndDate), attributes.EndDate);
+                Populate(dict, nameof(attributes.LicenseId), attributes.LicenseId.ToString());
+                Populate(dict, nameof(attributes.CustomerId), attributes.CustomerId.ToString());
+
+                if (dict.Count > 0)
+                {
+                    license = license.WithAdditionalAttributes(dict);
+                }
+            }
+        }
+
+        static void Populate(Dictionary<string, string> dict, string key, string value)
+        {
+            if (!string.IsNullOrEmpty(key)) dict[key] = value;
+        }
+
+        public static License Load(string file)
+        {
+            using (var fs = File.OpenRead(file))
+            {
+                return fs != null ? License.Load(fs) : null;
+            }
+        }
+
+        public static void Save(License license, string file)
+        {
+            File.WriteAllText(file, license?.ToString(), Encoding.UTF8);
+        }
+
+        public static void Show(License license)
+        {
+            Console.WriteLine(license?.ToString());
+        }
+
+        public static List<IValidationFailure> Validate(License license, string publicKey)
+        {
+            if (license == null || string.IsNullOrEmpty(publicKey))
+                return new List<IValidationFailure> { new GeneralValidationFailure { Message = "Empty license or key" } };
+            try
+            {
+                return license.Validate()
+                    .ExpirationDate()
+                    .When(lic => lic.Type == LicenseType.Trial)
+                    .And()
+                    .Signature(publicKey)
+                    .AssertValidLicense()
+                    .ToList() ?? new List<IValidationFailure>();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error in license validation: {e}");
+                return new List<IValidationFailure> { new GeneralValidationFailure { Message = e.Message } };
+            }
+        }
+
+        public static bool IsValid(License license, string publicKey)
+        {
+            return Validate(license, publicKey).Count == 0;
+        }
+
+        public static List<string> Errors(License license, string publicKey)
+        {
+            return Validate(license, publicKey).Select(failure => failure.GetType().Name + ": " + failure.Message + " - " + failure.HowToResolve).ToList();
+        }
+
+        public static LicenseParameters GetLicenseAttributes(License license)
+        {
+            var parameters = new LicenseParameters();
+
+            try
+            {
+                var features = license.ProductFeatures;
+                var attributes = license.AdditionalAttributes;
+
+                var feature = features.Get(nameof(parameters.Module));
+                if (feature != null)
+                {
+                    parameters.Module = feature;
+                }
+
+                feature = features.Get(nameof(parameters.Package));
+                if (feature != null)
+                {
+                    parameters.Package = feature;
+                }
+
+                feature = features.Get(nameof(parameters.Version));
+                if (feature != null)
+                {
+                    parameters.Version = feature;
+                }
+
+                feature = attributes.Get(nameof(parameters.Host));
+                if (feature != null)
+                {
+                    parameters.Host = feature;
+                }
+
+                var customer = license.Customer;
+                if (customer != null)
+                {
+                    parameters.Customer = customer.Name;
+                    parameters.Email = customer.Email;
+                }
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error in reading license attributes: {e}");
+            }
+
+            return parameters;
+        }
+    }
+
+}
---
+++ Solution/StandardLib/FromLicenseManager/ApplicationLicenseExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LicenseManager.Library
+{
+    public static class ApplicationLicenseExtensions
+    {
+        public static bool IsValidModule(this LicenseParameters parameters, string parameter)
+        {
+            return parameter == parameters.Module;
+        }
+
+        public static bool IsValidPackage(this LicenseParameters parameters, string parameter)
+        {
+            return parameter == parameters.Package;
+        }
+
+        public static bool IsValidVersion(this LicenseParameters parameters, int major, int minor)
+        {
+            var version = parameters.Version.Split('.');
+            if (version.Length != 2) return false;
+
+            if (!int.TryParse(version[0], out var majorVersion) || major > majorVersion)
+            {
+                return false;
+            }
+
+            if (!int.TryParse(version[1], out var minorVersion) || minor > minorVersion)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public static bool IsValidHost(this LicenseParameters parameters, string parameter)
+        {
+            return parameter == parameters.Host;
+        }
+    }
+}
